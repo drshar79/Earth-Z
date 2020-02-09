8a99eb27bd22bb209d0b00ccd8d492a4d4eb2688
Lumantere <54780722+Lumantere@users.noreply.github.com>
2020-02-08 19:04:50 -0500

    Revert "Revert "Update Project2.py""
    This reverts commit de3b76cc03affc16abf135ae8aff08d352a44a3a.

--- Project2.py
+++ Project2.py
@@ -3,25 +3,40 @@ def clear():
     # for windows 
     if name == 'nt': 
         _ = system('cls') 
-Name=input("Enter your name")
+Name=input("Enter your name: ")
 Start=input("Type in 'Start' to begin: ")
 if Start=="Start":
         clear()
-def downOrWindow():
-        ans = input()
-        if ans== "Go downstairs":
-                print("as you make your way downstairs, you notice that all is silent; no cars, no birds chirping, nothing except for the wind blowing against the house")
-                print("'This is strange,'thought"" "+Name,'.')
-        else:
-                print("That isn't one of your choices! Do you want to go downstairs, or look out the window?")
-                downOrWindow()
 print('You wake up with a start after a long and dreamless sleep.')
-print("'Time for another greuling day of medical school...Ugh.' You groan as you get out of bed")
+print("'Time for another greuling day of medical school...Ugh.' You groan as you get out of bed.")
 print("Do you...")
 print("Go downstairs")
 print("Look out the window")
-if ans=="Look out the window":
-        print("You open you window blinds and look out into the foggy morning.")
-        print("")
+def downOrWindow():
+        ans = input()
+        if ans== "Go downstairs":
+                clear()
+                print("as you make your way downstairs, you notice that all is silent; no cars, no birds chirping, nothing except for the soft wind blowing against the house.")
+                print("'This is strange,'thought"" "+Name,'.')
+                print("Do you...")
+                print("Go out the front door")
+                print("Go out the back door")
+        elif ans=="Look out the window":
+                        clear()
+                        print("You open you window blinds and look out into the foggy morning.")
+                        print("You stare in horror, as you witness a man eating the flesh of a near-dead man; their screams becoming softer and softer. ")
+                        print("Do you...")
+                        print("Continue to stare")
+                        print("Grab a weapon")
+                        def StareOrweapon():
+                                ans=input()
+                                if ans=="Continue to stare":
+                                        print("You keep staring. A few moments later, the 'man' turns and looks at you, their lifeless eyes leaving you shivering in your spine")  
+                        StareOrweapon()
 
+        else:
+                print("That isn't one of your choices! Do you want to...")
+                print("Go downstairs")
+                print("Look out the window")
+                downOrWindow()
 downOrWindow()
